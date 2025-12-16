a68f3e48898d9edd1b39923bd3f15b653c33ed47
DmitryStoyan <jombee@yandex.ru>
2025-12-16 16:45:06 +0300

    feat(ui): create UiCard component

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
+useHead({
+  title: 'Home | SRM System'
+})
 </script>
 
 <template>
   <div class="p-10">
     <h1 class="font-bold text-2xl mb-10">SRM System</h1>
-    <UiCard title="Zagolovok" price="100000 Р" subtitle="Описание проекта на самый клевый лад" date="10.12.2025" />
+    <UiCard draggable="true" title="Zagolovok" price="100000 Р" subtitle="Описание проекта на самый клевый лад"
+      date="10.12.2025" />
 
   </div>
 </template>
